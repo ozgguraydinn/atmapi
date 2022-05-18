e159be66a3706bab89b62c695ba5143ec74fd6fc
ozgguraydinn <ozgguraydinn@gmail.com>
2022-05-18 19:35:14 +0300

    Wallet eklendi.

--- src/api.go
+++ src/api.go
@@ -15,6 +15,9 @@ func api() {
 	r.HandleFunc("/", healthCheck)
 	r.HandleFunc("/users/login", UserLogin)
 	r.HandleFunc("/users/logout", UserLogout)
+	r.HandleFunc("/users/create", UserCreate)
+	r.HandleFunc("/wallet/transfer", BalanceTransfer)
+	r.HandleFunc("/wallet/list", WalletList)
 	http.ListenAndServe(":8080", r)
 }
 
@@ -39,6 +42,20 @@ func UserLogin(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func WalletList(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		walletList := Wallets()
+		jsonString, err := json.Marshal(walletList)
+		checkError(err)
+		fmt.Fprintf(w, string(jsonString))
+
+	} else {
+		w.Write([]byte("Desteklenmeyen method: " + r.Method))
+	}
+
+}
 func UserLogout(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Path)
 	if r.Method == "POST" {
@@ -51,6 +68,55 @@ func UserLogout(w http.ResponseWriter, r *http.Request) {
 			result = "Başarılı çıkış"
 		} else {
 			result = "Başarısız çıkış"
+		}
+		jsonString, err := json.Marshal(result)
+		checkError(err)
+		fmt.Fprintf(w, string(jsonString))
+
+	} else {
+		w.Write([]byte("Desteklenmeyen method: " + r.Method))
+	}
+
+}
+
+func UserCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+
+		userId := CreateUser(r.FormValue("username"), r.FormValue("password"))
+		var result string
+		if userId > 0 {
+			result = "Kullanıcı başarıyla oluşturuldu: ID = " + strconv.Itoa(int(userId))
+			walletPin, err := CreateWallet(userId, r.FormValue("PIN"))
+			checkError(err)
+			result += " PIN = " + walletPin
+		} else {
+			result = "Başarısız işlem"
+		}
+		jsonString, err := json.Marshal(result)
+		checkError(err)
+		fmt.Fprintf(w, string(jsonString))
+
+	} else {
+		w.Write([]byte("Desteklenmeyen method: " + r.Method))
+	}
+
+}
+func BalanceTransfer(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		userId, err := strconv.Atoi(r.FormValue("id"))
+		checkError(err)
+		amount, err := strconv.Atoi(r.FormValue("amount"))
+		checkError(err)
+		userControl := BalanceTransferToCardNo(int64(userId), r.FormValue("cardNoSender"), r.FormValue("pin"), r.FormValue("cardNorecipient"), int64(amount))
+		var result string
+		if userControl {
+			result = "İşlem başarılı"
+		} else {
+			result = "İşlem başarısız"
 		}
 		jsonString, err := json.Marshal(result)
 		checkError(err)
--- src/db.go
+++ src/db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"math/rand"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -25,16 +26,19 @@ func databaseConnect() {
 }
 
 func tableCreate(db *sql.DB) {
-	db.Exec("CREATE TABLE if not exists users ( Id INTEGER NOT NULL, Username text UNIQUE, Password text, IsOnline INTEGER, PRIMARY KEY(Id AUTOINCREMENT) )")
-	db.Exec("CREATE TABLE sqlite_sequence(name,seq)")
-	db.Exec("INSERT INTO sqlite_sequence VALUES(users,0)")
-
+	db.Exec("CREATE TABLE if not exists users ( Id INTEGER NOT NULL, Username text UNIQUE NOT NULL, Password text NOT NULL, IsOnline INTEGER NOT NULL, PRIMARY KEY(Id AUTOINCREMENT) )")
+	db.Exec("CREATE TABLE if not exists wallet ( UserId INTEGER NOT NULL, CardNo	TEXT NOT NULL, Balance	INTEGER NOT NULL, PIN	TEXT NOT NULL, PRIMARY KEY(CardNo), UNIQUE(UserId))")
 }
 
 type User struct {
 	Id       int
 	Username string
 	IsOnline int
+}
+type wallet struct {
+	UserId  int
+	CardNo  string
+	Balance int
 }
 
 func Login(username string, password string) User {
@@ -46,9 +50,8 @@ func Login(username string, password string) User {
 	if err == nil {
 		for rows.Next() {
 			var id int
-			var username string
 			var isOnline int
-			err = rows.Scan(&id, &username, &isOnline)
+			err = rows.Scan(&id, &isOnline)
 			if err == nil {
 				userList = append(userList, User{Id: id, Username: username, IsOnline: isOnline})
 			} else {
@@ -67,14 +70,41 @@ func Login(username string, password string) User {
 			if err == nil {
 				user = userList[0]
 			}
+		} else if len(userList) > 0 {
+			user = userList[0]
 		}
 	} else {
 		fmt.Println(err)
 	}
 	return user
 }
+
+func Wallets() []wallet {
+	var walletList []wallet
+	walletList = nil
+	stmt, err := db.Prepare("select UserId,CardNo,Balance from wallet")
+	rows, err := stmt.Query()
+	if err == nil {
+		for rows.Next() {
+			var userId int
+			var cardNo string
+			var balance int
+			err = rows.Scan(&userId, &cardNo, &balance)
+			if err == nil {
+				walletList = append(walletList, wallet{UserId: userId, CardNo: cardNo, Balance: balance})
+			} else {
+				fmt.Println(err)
+			}
+		}
+		rows.Close()
+	} else {
+		fmt.Println(err)
+	}
+	return walletList
+}
+
 func Logout(id int) bool {
-	stmt, err := db.Prepare("update users set IsOnline=0 where Id = ?")
+	stmt, err := db.Prepare("update users set IsOnline=0 where Id = ? and IsOnline = 1")
 	checkError(err)
 	fmt.Println(id)
 	res, err := stmt.Exec(id)
@@ -86,45 +116,46 @@ func Logout(id int) bool {
 	}
 	return false
 }
-func addDictionary(word, wordMean string) {
-
-	stmt, err := db.Prepare("INSERT INTO tblSozluk(kelime, kelimeAnlam) values (?,?)")
+func CreateUser(username string, password string) int64 {
+	stmt, err := db.Prepare("INSERT OR IGNORE  users(Username, Password, IsOnline) values (?,?,0)")
 	checkError(err)
-	res, err := stmt.Exec(word, wordMean)
+	res, err := stmt.Exec(username, password)
 	checkError(err)
-
 	id, err := res.LastInsertId()
 	checkError(err)
-
-	fmt.Println("Son eklenen kayıt: ", id)
-
+	return id
 }
-
-func deleteDictionary(wordID int) {
-
-	stmt, err := db.Prepare("DELETE FROM TBLsOZLUK where kelimeID=?")
+func CreateWallet(id int64, pin string) (string, error) {
+	stmt, err := db.Prepare("INSERT OR IGNORE INTO wallet(UserId,CardNo,Balance,PIN ) VALUES(?,?,100,?)")
 	checkError(err)
-	res, err := stmt.Exec(wordID)
+	fmt.Println(id)
+	res, err := stmt.Exec(id, rand.Intn(999999), pin)
 	checkError(err)
-
 	_, err = res.RowsAffected()
 	checkError(err)
-
-	fmt.Println("Kayıt silindi")
-
+	if err == nil {
+		return "Kart oluşturuldu. PIN = " + pin, err
+	}
+	return "Kart oluşturulamadı", err
 }
-
-func updateDictionary(wordID int, word, wordMean string) {
-
-	stmt, err := db.Prepare("update tblSoz set kelime=? .....................")
+func BalanceTransferToCardNo(id int64, cardNoSender string, pin string, cardNorecipient string, amount int64) bool {
+	stmt, err := db.Prepare("update wallet set Balance = Balance - ? where UserId = ? and CardNo = ? and PIN = ? and Balance >= ?")
 	checkError(err)
-
-	res, err := stmt.Exec(word, wordMean, wordID)
+	res, err := stmt.Exec(amount, id, cardNoSender, pin, amount)
 	checkError(err)
-
-	_, err = res.RowsAffected()
+	affectedRow, err := res.RowsAffected()
 	checkError(err)
-
-	fmt.Println("Veri güncellendi")
-
+	if err == nil && affectedRow > 0 {
+		stmt2, err2 := db.Prepare("update wallet set Balance = Balance + ? where CardNo = ? ")
+		checkError(err2)
+		fmt.Println(id)
+		res2, err2 := stmt2.Exec(amount, cardNorecipient)
+		checkError(err2)
+		affectedRow2, err2 := res2.RowsAffected()
+		checkError(err2)
+		if err2 == nil && affectedRow2 > 0 {
+			return true
+		}
+	}
+	return false
 }
